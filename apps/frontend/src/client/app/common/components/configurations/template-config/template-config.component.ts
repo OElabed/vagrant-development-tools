abd49cc88b057fe22e05d6e9f931743d84903f31
oelabed <oel3abed@gmail.com>
2017-11-10 00:44:01 +0100

    form validation

--- apps/frontend/src/client/app/common/components/configurations/template-config/template-config.component.ts
+++ apps/frontend/src/client/app/common/components/configurations/template-config/template-config.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, Input, Output, EventEmitter, AfterViewInit } from '@angular/core';
-import { FormBuilder, FormGroup, FormControl, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, FormControl, Validators, NgForm } from '@angular/forms';
 
 import { ICommonEnvConfig, IPackageFormConfig, CommonEnvConfig, PackageFormConfig } from '../../../models/view/package-config.model';
 import { IAddFileFormConfig, AddFileFormConfig, AddFileFormType } from '../../../models/view/add-file-config.model';
@@ -27,14 +27,48 @@ export class TemplateConfigComponent implements AfterViewInit, OnInit {
 
     ngOnInit() {
         this.templateForm = new FormGroup({
-            'name': new FormControl('', Validators.required),
-            'birthYear': new FormControl('', [Validators.required])
+            'general': new FormGroup({
+                'plateform': new FormControl('', Validators.required),
+                'name': new FormControl('', Validators.required)
+            }),
+            'coreEngine': new FormGroup({
+                'version': new FormControl('', Validators.required)
+            }),
+            'filterEngine': new FormGroup({
+                'version': new FormControl('', Validators.required),
+                'filterEngineOptions': new FormGroup({
+                    'fmlFile1': new FormControl(''),
+                    'fmlFile2': new FormControl(''),
+                    'scoreFile': new FormControl(''),
+                })
+            }),
+            'continuityBackend': new FormGroup({
+                'version': new FormControl('', Validators.required),
+                'push': new FormControl('', Validators.required)
+            }),
+            'database': new FormGroup({
+                'type': new FormGroup({
+                    'oracle': new FormControl(''),
+                    'sqlServer': new FormControl(''),
+                    'db2': new FormControl('')
+                }),
+                'username': new FormControl('', Validators.required),
+                'password': new FormControl('', Validators.required),
+                'hostname': new FormControl('', Validators.required),
+                'port': new FormControl('', Validators.required),
+                'service': new FormControl('', Validators.required)
+            })
         });
     }
 
     ngAfterViewInit(): void {
         jQuery('.selecttemplate').selectpicker();
         jQuery('.selectplateform').selectpicker();
+    }
+
+    validation(templateForm: NgForm) {
+        console.log('Registration successful.');
+        console.log(templateForm.value);
     }
 
 }

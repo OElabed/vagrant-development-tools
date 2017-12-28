0d66a4a3f04ecb6da53a17745a95f6ea148c8091
oelabed <oel3abed@gmail.com>
2017-12-28 01:04:36 +0100

    add changes

--- apps/server/src/main/java/com/fix/config/OAuth2AuthorizationServerConfig.java
+++ apps/server/src/main/java/com/fix/config/OAuth2AuthorizationServerConfig.java
@@ -2,7 +2,6 @@ package com.fix.config;
 
 import com.fix.service.CustomUserDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
--- apps/server/src/main/java/com/fix/config/OAuth2ResourceServerConfig.java
+++ apps/server/src/main/java/com/fix/config/OAuth2ResourceServerConfig.java
@@ -4,16 +4,12 @@ import com.fix.service.CustomUserDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.access.expression.method.MethodSecurityExpressionHandler;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
-import org.springframework.security.config.annotation.method.configuration.GlobalMethodSecurityConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
-import org.springframework.security.oauth2.provider.expression.OAuth2MethodSecurityExpressionHandler;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
 /**

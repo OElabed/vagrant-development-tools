90c5877e91c3853c84a68b5b68324226d8455d2e
oelabed <oel3abed@gmail.com>
2018-01-07 21:19:40 +0100

    link frontend with backend

---
+++ apps/server/src/main/java/com/fix/config/SimpleCorsFilter.java
@@ -0,0 +1,7 @@
+package com.fix.config;
+
+/**
+ * Created by OELABED on 06/01/2018.
+ */
+public class SimpleCorsFilter {
+}
---
+++ apps/server/src/main/java/com/fix/config/WebSecurityConfig.java
@@ -0,0 +1,12 @@
+package com.fix.config;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by OELABED on 06/01/2018.
+ */
+@Configuration
+public class CorsConfig {
+
+
+}

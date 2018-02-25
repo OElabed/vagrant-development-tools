cda59a05c3d0cd75258505bac02e54983938ba33
oelabed <oel3abed@gmail.com>
2018-02-25 23:23:40 +0100

    delete fix-backend

--- apps/fix-backend/webui/src/client/app/common/models/domain/database.model.ts
+++
@@ -1,5 +0,0 @@
-export enum DatabaseType {
-    ORACLE = 'ORACLE',
-    SQLSERVER = 'SQLSERVER',
-    DB2 = 'DB2'
-}
--- apps/fix-backend/webui/src/client/app/common/utils/file.utils.ts
+++
@@ -1,9 +0,0 @@
-export class FileUtils {
-
-    public static formatBytes(bytes: number): string {
-        if (bytes < 1024) return bytes + ' Bytes';
-        else if (bytes < 1048576) return (bytes / 1024).toFixed(3) + ' KB';
-        else if (bytes < 1073741824) return (bytes / 1048576).toFixed(3) + ' MB';
-        else return (bytes / 1073741824).toFixed(3) + ' GB';
-    }
-}

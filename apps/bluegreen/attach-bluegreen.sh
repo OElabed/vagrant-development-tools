4fcebd807801245c38c56e4a3eac6d11af26620b
oelabed <oel3abed@gmail.com>
2018-06-02 19:42:03 +0200

    delete blue green

--- apps/bluegreen/attach-bluegreen.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-CONTAINER_ID="$(sudo docker ps -aqf "name=bluegreen_bluegreen_1")"
-
-sudo  docker exec -i -t --env COLUMNS=`tput cols` --env LINES=`tput lines` $CONTAINER_ID /bin/bash
--- apps/bluegreen/setup-bluegreen.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-sudo docker-compose build bluegreen
-
-sudo docker-compose up -d bluegreen
